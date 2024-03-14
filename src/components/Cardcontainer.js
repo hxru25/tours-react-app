b6e8bb5d91255fa05e0fc19c441b62ca1ca2f016
hxru25 <harinilive@gmail.com>
2024-03-14 10:44:43 +0530

    my first commit

---
+++ src/components/Cardcontainer.js
@@ -0,0 +1,20 @@
+import Card from "./Card";
+import data from "../database/data";
+const Cardcontainer = ({data}) => {
+    return (
+        <div className="filter-container">
+            <div className="cards">
+                {
+                    data.map((item) => (
+                        <Card key={item.id}{...item}></Card>
+                    ))
+                }
+
+
+            </div>
+   
+        </div>
+    )
+}
+
+export default Cardcontainer;
